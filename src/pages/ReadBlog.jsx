95627cf6b58c5de12d3c7cbf381728e06580e8a7
Chokseys2022 <choksey2022@gmail.com>
2024-03-22 12:53:33 -0700

    read page renders from navbar

--- src/pages/ReadBlog.jsx
+++ src/pages/ReadBlog.jsx
@@ -5,10 +5,12 @@ function ReadBlog() {
 
   useEffect(() => {
     const hash = window.location.hash;
+    if(hash){
     const element = document.querySelector(hash);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
       setSelectedSection(hash);
+    }
     }
   }, []);
 
