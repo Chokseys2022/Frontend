ed0d993bf2cb13e6a34c800618203e1c1685a39f
Chokseys2022 <choksey2022@gmail.com>
2024-03-22 15:13:49 -0700

    buttons in searchblog fixed

--- src/pages/SearchBlog.jsx
+++ src/pages/SearchBlog.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import danceImage from "../images/dance.jpg";
+import famImage from "../images/fam.jpg";
+import thoughtImage from "../images/thought.jpg";
+import foodImage from "../images/food.png";
 
 function SearchBlog() {
   return (
     <div>
       <h1>SEARCH</h1>
+
       <div className="button-container">
         <NavLink to="/read#dance-section">
-          <img src={"./dance.jpg"} alt="dance" />
+          <img className="image" src={danceImage} alt="dance" />
         </NavLink>
+
         <NavLink to="/read#fam-section">
-          <img src="fam.jpg" alt="family" />
+          <img className="image" src={famImage} alt="family" />
         </NavLink>
+
         <NavLink to="/read#thought-section">
-          <img src="thought.jpg" alt="reflection" />
+          <img className="image" src={thoughtImage} alt="reflection" />
         </NavLink>
+
         <NavLink to="/read#food-section">
-          <img src="food.png" alt="food" />
+          <img className="image" src={foodImage} alt="food" />
         </NavLink>
       </div>
     </div>
