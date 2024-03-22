2b3683e075da0b824a5a66c57a397bbffd5e2d18
Chokseys2022 <choksey2022@gmail.com>
2024-03-21 20:42:13 -0700

    tried to add images

--- src/pages/SearchBlog.jsx
+++ src/pages/SearchBlog.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function SearchBlog() {
   return (
     <div>
       <h1>SEARCH</h1>
+      <div className="button-container">
+        <NavLink to="/read">
+          <img src={"./dance.jpg"} alt="Button 1" />
+          {/* <img src={('./images/dance.jpg')} /> */}
+        </NavLink>
+        <NavLink to="/read">
+          <img src="fam.jpg" alt="Button 2" />
+        </NavLink>
+        <NavLink to="/read">
+          <img src="thought.jpg" alt="Button 3" />
+        </NavLink>
+        <NavLink to="/read">
+          <img src="food.png" alt="Button 4" />
+        </NavLink>
+      </div>
     </div>
   );
 }
 export default SearchBlog;
-
-//useState - images and summary about blog that link to Read Blog
