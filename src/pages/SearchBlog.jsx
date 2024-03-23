027b2e45aaceff46120a5775df7580cdcc262bd0
Chokseys2022 <choksey2022@gmail.com>
2024-03-23 10:53:23 -0700

    changed searchpage

--- src/pages/SearchBlog.jsx
+++ src/pages/SearchBlog.jsx
@@ -4,34 +4,33 @@ import danceImage from "../images/dance.jpg";
 import famImage from "../images/fam.jpg";
 import thoughtImage from "../images/thought.jpg";
 import foodImage from "../images/food.png";
+import "../App.css"; // Import CSS for styling
 
 function SearchBlog() {
   return (
-    <div>
-      <h1 style ={{textAlign:'center'}}>SEARCH</h1>
+    <div className="search-blog-container">
+      <h1 className="search-blog-heading">SEARCH</h1>
 
       <div className="button-container">
-        <NavLink to="/read#dance-section">
+        <NavLink to="/read#dance-section" className="blog-link">
           <img className="image" src={danceImage} alt="dance" />
-          <div>Dance</div>
+          <div className="description">Explore articles on various dance forms, tutorials, and dance culture.</div>
         </NavLink>
 
-        <NavLink to="/read#fam-section">
+        <NavLink to="/read#fam-section" className="blog-link">
           <img className="image" src={famImage} alt="family" />
-          <div>Family</div>
+          <div className="description">Discover heartwarming stories, tips on parenting, and family bonding activities.</div>
         </NavLink>
 
-        <NavLink to="/read#thought-section">
+        <NavLink to="/read#thought-section" className="blog-link">
           <img className="image" src={thoughtImage} alt="reflection" />
-          <div>Reflection</div>
+          <div className="description">Engage in thoughtful reflections, personal essays, and philosophical discussions.</div>
         </NavLink>
 
-        <NavLink to="/read#food-section">
+        <NavLink to="/read#food-section" className="blog-link">
           <img className="image" src={foodImage} alt="food" />
-          <div>Food</div>
+          <div className="description">Indulge in culinary delights, recipes from around the world, and foodie adventures.</div>
         </NavLink>
-
-        
       </div>
     </div>
   );
